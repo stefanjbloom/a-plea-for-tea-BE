35d9cf8c45309bbc9d37321060994539e1b30765
Stefan Bloom <stefanjbloom88@gmail.com>
2024-11-12 13:35:46 -0700

    Test: all model associations and validations green

--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -1,5 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe Customer, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validations' do
+    it { should validate_presence_of(:firstname)  }
+    it { should validate_presence_of(:lastname)  }
+    it { should validate_presence_of(:email)  }
+    it { should validate_presence_of(:address)  }
+    it { should validate_length_of(:firstname).is_at_most(40) }
+    it { should validate_length_of(:lastname).is_at_most(40) }
+    it { should_not allow_value("test").for(:email) }
+    it { should_not allow_value("test.com").for(:email) }
+    it { should allow_value("test@example.com").for(:email) }
+    it { should validate_uniqueness_of(:email)  }
+  end
+
+  describe 'associations' do
+    it { should have_many(:subscriptions) }
+    it { should have_many(:teas).through(:subscriptions) }
+  end
 end
--- spec/models/subscription_spec.rb
+++ spec/models/subscription_spec.rb
@@ -1,5 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe Subscription, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validations' do
+    it { should validate_presence_of(:customer_id)  }
+    it { should validate_presence_of(:tea_id)  }
+    it { should validate_presence_of(:title)  }
+    it { should validate_presence_of(:price)  }
+    it { should validate_presence_of(:status) }
+    it { should validate_presence_of(:frequency) }
+    it { should validate_numericality_of(:customer_id).only_integer }
+    it { should validate_numericality_of(:tea_id).only_integer }
+    it { should validate_numericality_of(:price) }
+    it { should validate_inclusion_of(:status).in_array(%w(active canceled)).with_message("Invalid Status") }
+  end
+
+  describe 'associations' do
+    it { should belong_to(:customer) }
+    it { should belong_to(:tea) }
+  end
 end
