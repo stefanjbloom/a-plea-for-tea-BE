7765e47e926a464a48e7036f4710162d8964c4e7
Stefan Bloom <stefanjbloom88@gmail.com>
2024-11-12 11:51:16 -0700

    feat:customer associations and validations.  all rspec tests for models generated

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,3 +1,10 @@
 class Customer < ApplicationRecord
-  
+    has_many :subscriptions
+    has_many :teas, through: :subscriptions
+
+    validates :firstname, :lastname, :email, :address, presence: true
+    validates :firstname, length: { maximum: 40}
+    validates :lastname, length: { maximum: 40}
+    validates :email, format: { with: /\A(.+)@(.+)\z/, message: "Invalid Email Address"  }
+    validates :email, uniqueness: true
 end
